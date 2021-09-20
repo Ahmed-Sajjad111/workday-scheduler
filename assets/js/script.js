2c7e503fba998024bb20b1e2cba7a012832f65ae
Ahmed Sajjad <uasajjad11198@outlook.com>
2021-09-20 02:32:06 -0400

    added ability to click in hour slots and enter text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,15 @@
 let currentDate = moment().format("dddd, MMM Do");
 $("#currentDay").text(currentDate);
+
+let tasks = {};
+
+$(".time-block").on("click", "div", function() {
+    let taskId = $(this).attr("id").replace("row", "");
+    let taskDivEl = $("#" + taskId);
+    let taskClass = taskDivEl.attr("class");
+    let taskText = taskDivEl.text();
+    let textInput = $("<textarea>").addClass(taskClass).val(taskText);
+    textInput.attr("id", taskId);
+    taskDivEl.replaceWith(textInput);
+    textInput.trigger("focus");
+})
