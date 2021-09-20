6363320a7d4684a62e14603a16acdf3e8b9dba57
Ahmed Sajjad <uasajjad11198@outlook.com>
2021-09-20 03:14:40 -0400

    added save button feature to push to object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,46 @@
 let currentDate = moment().format("dddd, MMM Do");
 $("#currentDay").text(currentDate);
 
-let tasks = {};
+let hourlyTasks = {};
 
 $(".time-block").on("click", "div", function() {
-    let taskId = $(this).attr("id").replace("row", "");
+    let taskId = $(this)
+        .attr("id")
+        .replace("row", "");
     let taskDivEl = $("#" + taskId);
-    let taskClass = taskDivEl.attr("class");
-    let taskText = taskDivEl.text();
-    let textInput = $("<textarea>").addClass(taskClass).val(taskText);
-    textInput.attr("id", taskId);
+    let taskClass = taskDivEl
+        .attr("class");
+    let taskText = taskDivEl
+        .text();
+    let textInput = $("<textarea>")
+        .attr("id", taskId);
+    textInput.addClass(taskClass)
+        .val(taskText);
     taskDivEl.replaceWith(textInput);
     textInput.trigger("focus");
+})
+
+$(".time-block").on("click", ".saveBtn", function () {
+    let id = $(this)
+        .attr("id")
+        .replace("btn", "");
+    let textEl = $("#div" + id)
+    let taskText = textEl
+        .val()
+        .trim();
+    if (!taskText) {
+        taskText = textEl
+            .text()
+            .trim();
+    };
+    let divId = $("<div>")
+        .attr("id", "div" + id)
+        .addClass(textEl.attr("class"))
+        .text(taskText)
+        console.log(textEl)
+        console.log(divId)
+    textEl.replaceWith(divId);
+    console.log(textEl)
+    tasks[id] = taskText;
+    console.log(hourlyTasks)
 })
